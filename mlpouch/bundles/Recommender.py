9281b0b94f1e1be76b38878bf167749fd1b760a9
Marc Vitalis <marc.vitalis@live.com>
2021-09-30 16:08:50 +0800

    finish bundle

--- mlpouch/bundles/Recommender.py
+++ mlpouch/bundles/Recommender.py
@@ -1,2 +1,28 @@
+import numpy as np
+
 class Recommender:
-    pass
+    
+    def __init__(self, recommenders):
+        self.recommenders = recommenders
+
+    def recommend(self, U, rec_num=5):
+        results = []
+
+        for U_item in U:
+            results.append(self._recommend(U_item, rec_num))
+
+        return results
+        
+
+    def _recommend(self, U_item, rec_num=5):
+
+        r = [U_item, np.NaN]
+
+        for rec in self.recommenders:
+            result = rec.recommend([U_item], rec_num)
+
+            if result[0][1] != np.NaN:
+                r = result[0][1]
+                break
+
+        return r
